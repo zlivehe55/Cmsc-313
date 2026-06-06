94eafb1d3dc41b26a1d399db456a4cabddb4c2f7
zlievhe <71079664+zlivehe55@users.noreply.github.com>
2026-06-06 10:09:48 -0400

    Update Hexdump.c

--- project3/Hexdump.c
+++ project3/Hexdump.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("usage: %s filename\n", argv[0]);
+        printf("the usage: %s filename\n", argv[0]);
         return 1;
     }
 
